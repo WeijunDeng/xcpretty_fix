e879197a4ad3d8f13f6209e1448789f14998ee6e
dengweijun <dwjchn@gmail.com>
2022-07-28 17:08:43 +0800

    fix

--- scripts/xcpretty_fix.py
+++ scripts/xcpretty_fix.py
@@ -1,16 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env /usr/bin/python
 
 import sys
 import re
 
 has_undefined = False
 while True:
-    line = sys.stdin.readline()
+    line = sys.stdin.readline().strip("\n")
     if len(line) == 0:
         break
-    if line.startswith("Undefined symbols") or has_undefined:
+    if line.startswith("Undefined symbols"):
         has_undefined = True
-        line = line.replace("Undefined symbols", "ld: Undefined symbols")
-        line = re.sub(r'(.*, referenced from:)', r'ld: \1', line)
-        line = re.sub(r'(.* in .*\(.*\.o\))', r'ld: \1', line)
+    elif has_undefined and not line.startswith(" "):
+        has_undefined = False
+    if has_undefined:
+        line = "ld: " + line
     print(line)
